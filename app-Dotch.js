66b370386cc7859e54b27f337e6d9542b07c939b
IkehAkinyemi <mrikehchukwka@gmail.com>
2020-08-27 01:07:37 +0100

    UI changes

--- app-Dotch.js
+++ app-Dotch.js
@@ -1,13 +1,33 @@
+//DOM variables
 let bodyUi = document.body;
-console.log(bodyUi);
+let navContainer = document.querySelector('.nav-container'),
+    searchInput = document.querySelector('#search-input'),
+    menu = document.querySelector('.menu');
+//Local variables for javascript logic
+let count = 0; // using the count varaible to keep track of when to close and open the search input.
 
-bodyUi.addEventListener('touchstart', this.touchstart);
-bodyUi.addEventListener('touchmove', this.touchmove);
 
-function touchstart(e) {
-    e.preventDefault()
-}
+//Event listener attached unto the body of the document.
+bodyUi.addEventListener('click', openOrCloseSearch);
+bodyUi.addEventListener('click', closeMenu);
 
-function touchmove(e) {
-    e.preventDefault()
+//Function to open and close the search input at the top of document.
+function openOrCloseSearch(e) {
+    if (e.target.classList.contains('fa-search') && count % 2 == 0) {
+        count++;
+        navContainer.style.marginTop = '6rem'
+        searchInput.style.display = 'block';
+    } else {
+        if (e.target.classList.contains('fa-search') &&count % 2 != 0) {
+            searchInput.style.display = 'none';
+            navContainer.style.marginTop = '0rem'
+            count++;
+        }
+    }
+};
+
+function closeMenu(e) {
+    if (e.target.classList.contains('fa-bars')) {
+        menu.style.display = 'block';
+    }
 }
