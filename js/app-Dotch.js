0b08b684f5c44056138e12c561e20f0cc7ee1064
IkehAkinyemi <mrikehchukwka@gmail.com>
2020-09-02 02:18:40 +0100

    file path update

---
+++ js/app-Dotch.js
@@ -0,0 +1,56 @@
+//DOM variables
+let bodyUi = document.body;
+let navContainer = document.querySelector('.nav-collection'),
+    searchInput = document.querySelector('#search-input'),
+    header = document.querySelector('header')
+    menu = document.querySelector('.menu');
+//Local variables for javascript logic
+let searchCount = 0, // using the searchCount varaible to keep track of when to close and open the search input.
+    menuCount = 0; // using the menuCount varaible to keep track of when to close and open the menu.
+
+
+//Event listener attached unto the body of the document.
+navContainer.addEventListener('click', openOrCloseSearch);
+header.addEventListener('click', closeMenu);
+searchInput.addEventListener('focus', () => {
+    console.log('Hey');
+    searchInput.style.border = 'none';
+});
+
+//Function to open and close the search input at the top of document.
+function openOrCloseSearch(e) {
+    if (e.target.classList.contains('fa-search') && searchCount % 2 == 0) {
+        searchCount++;
+        navContainer.style.transition = 'all .5s ease';
+        // setTimeout(function() {
+        //     navContainer.style.transition = 'none';
+        // }, 350)
+        searchInput.style.display = 'block';
+    } else {
+        if (e.target.classList.contains('fa-search') && searchCount % 2 != 0) {
+            navContainer.style.transition = 'all .5s ease';
+            searchInput.style.display = 'none';
+            setTimeout(function() {
+                navContainer.style.transition = 'none';
+            }, 200)
+            navContainer.style.marginTop = '0rem';
+            searchCount++;
+        }
+    }
+    e.preventDefault();
+};
+
+function closeMenu(e) {
+    if (e.target.classList.contains('fa-bars') && menuCount % 2 == 0) {
+        menu.style.display = 'block';
+        searchInput.style.display = 'none';
+        menuCount++;
+    } else {
+        if (e.target.classList.contains('fa-bars') && menuCount % 2 != 0) {
+            setTimeout(function() {
+                menu.style.display = 'none';
+            },250)
+            menuCount++;
+        }
+    }
+}
