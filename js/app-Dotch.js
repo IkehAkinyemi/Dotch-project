44f7b004a8c57b3e09e4b5f84639408f3768bfef
IkehAkinyemi <mrikehchukwka@gmail.com>
2020-09-03 22:22:01 +0100

    UI changes

--- js/app-Dotch.js
+++ js/app-Dotch.js
@@ -4,12 +4,14 @@ let navContainer = document.querySelector('.nav-collection'),
     searchInput = document.querySelector('#search-input'),
     header = document.querySelector('header')
     menu = document.querySelector('.menu');
-//Local variables for javascript logic
+
+
+//javascript flags for logic
 let searchCount = 0, // using the searchCount varaible to keep track of when to close and open the search input.
     menuCount = 0; // using the menuCount varaible to keep track of when to close and open the menu.
 
 
-//Event listener attached unto the body of the document.
+//Event listener attached unto elements of the document.
 navContainer.addEventListener('click', openOrCloseSearch);
 header.addEventListener('click', closeMenu);
 searchInput.addEventListener('focus', () => {
@@ -22,9 +24,6 @@ function openOrCloseSearch(e) {
     if (e.target.classList.contains('fa-search') && searchCount % 2 == 0) {
         searchCount++;
         navContainer.style.transition = 'all .5s ease';
-        // setTimeout(function() {
-        //     navContainer.style.transition = 'none';
-        // }, 350)
         searchInput.style.display = 'block';
     } else {
         if (e.target.classList.contains('fa-search') && searchCount % 2 != 0) {
@@ -32,11 +31,12 @@ function openOrCloseSearch(e) {
             searchInput.style.display = 'none';
             setTimeout(function() {
                 navContainer.style.transition = 'none';
-            }, 200)
+            }, 200);
             navContainer.style.marginTop = '0rem';
             searchCount++;
-        }
-    }
+        };
+    };
+
     e.preventDefault();
 };
 
@@ -49,8 +49,8 @@ function closeMenu(e) {
         if (e.target.classList.contains('fa-bars') && menuCount % 2 != 0) {
             setTimeout(function() {
                 menu.style.display = 'none';
-            },250)
+            },250);
             menuCount++;
         }
-    }
-}
+    };
+};
